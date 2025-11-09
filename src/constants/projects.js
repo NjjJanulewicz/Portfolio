60a39eb73dc88c5f61ec13a0f657c8d23fba6368
Nicholas Janulewicz <nicholas.janulewicz@gmail.com>
2025-11-09 15:21:48 -0600

    small project formatting changes

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -3,10 +3,10 @@ export const PROJECTS = [
     route: "rnr",
     name: "Rise & Reign",
     link: "https://rnr-client-two.vercel.app/",
-    description: "An Ecommerce site for Initially built for a fitness brand. It uses Django for the backend and React for the frontend." +
-      "It handles payments through Stripe and uses a custom payment gateway to handle payments." +
-      "Client funding for the project ran out before the project was completed, but the site is still live on Vercel." +
-      "The images on the site are stock images, so they may be a inaccurate representation",
+    description: "An Ecommerce site for Initially built for a fitness brand. It uses Django for the backend and React for the frontend. " +
+      "It handles payments through Stripe and uses a custom payment gateway to handle payments. " +
+      "Client funding for the project ran out before the project was completed, but the site is still live on Vercel. " +
+      "The images on the site are stock images, so they may be a inaccurate representation.",
     tags: ["React", "Redux", "SQL", "Stripe", "Django", "Python", "TypeScript", "Material UI"],
     titleImage: "/rnr/title.png",
     images: [
@@ -23,10 +23,10 @@ export const PROJECTS = [
     route: "dm",
     name: "Datamanager",
     link: null,
-    description: "This is a data management software that I worked on while employed at Elemental Scientific." +
-      "It serves as the main data management tool for the analytical station SampleTrax." +
-      "It has RFID integration for secure access.",
-    tags: ["SQLServer", "WPF", "dotnet"],
+    description: "This is a data management software that I worked on while employed at Elemental Scientific. " +
+      "It serves as the main data management tool for the analytical station SampleTrax. " +
+      "It has RFID integration for secure access. ",
+    tags: ["SQLServer", "WPF", ".NET", "C#"],
     titleImage: "/dm/title.png",
     images: [
       {src: "/dm/bottleFilter.png", description: "Bottle filter with async loading"},
@@ -40,11 +40,11 @@ export const PROJECTS = [
     route: "trax",
     name: "SampleTRAX",
     link: "https://icpms.com/products/barcode-reading/sampletrax-sample-identification-autosamplers/",
-    description: "This is an analytical chemistry software that I worked on while employed at Elemental Scientific." +
-      "It uses SQLServer and WFP. It is a fully featured software that aims to have an industry leading feature set" +
+    description: "This is an analytical chemistry software that I worked on while employed at Elemental Scientific. " +
+      "It uses SQLServer and WFP. It is a fully featured software that aims to have an industry leading feature set. " +
       "I've traveled from coast to coast in the US and to some parts of Asia in world leading semiconductor labs to see" +
       "the integration of our networked system into the real world.",
-    tags: ["SQLServer", "WPF", "dotnet", "C#", "Database Administration"],
+    tags: ["SQLServer", "WPF", ".NET", "C#", "Database Administration"],
     titleImage: "/trax/title.png",
     images: [
       {src: "/trax/qc.png", description: "Live Quality Control editing"},
@@ -57,13 +57,13 @@ export const PROJECTS = [
     name: "Parts Cruise",
     link: "https://parts-cruise.vercel.app/",
     description: "This was my capstone project for my undergraduate degree. " +
-      "It is a meta search engine for used automotive parts that uses React for the frontend and django for the backend." +
-      "It scrapes data from automotive forums and allows users to search for parts and add them to their cart." +
+      "It is a meta search engine for used automotive parts that uses React for the frontend and django for the backend. " +
+      "It scrapes data from automotive forums and allows users to search for parts and add them to their cart. " +
       "The motivation for this project was because at the time I was very passionate about cars but didn't have the money to buy new parts so " +
       "I often found myself wasting lots of time digging for what I needed so I made Parts Cruise to solve this problem." +
-      "Searching is supported by a custom search engine that uses Full-Text Search with an Inverted Index." +
-      "This project was completed in over a semester and was a great learning experience for me." +
-      "Note that the scope of this project didn't include a reactive UI so its only intended to be viewed on desktop.",
+      "Searching is supported by a custom search engine that uses Full-Text Search with an Inverted Index. " +
+      "This project was completed in over a semester and was a great learning experience for me. " +
+      "Note that the scope of this project didn't include a reactive UI so its only intended to be viewed on desktop. ",
     tags: ["React", "Redux", "SQL", "Python", "Django", "TypeScript", "Material UI"],
     titleImage: "/pc/title.png",
     images: [
@@ -79,11 +79,11 @@ export const PROJECTS = [
     route: "scan",
     name: "Scan Station",
     link: null,
-    description: "This is a lightweight barcode scanning station that I built while at Elemental Scientific." +
-      "It uses Entitiy Framework Core and WPF. It is a simple tool that allows users to use a barcode scanner to associate vial barcodes with a sample rack." +
-      "The application is very modular as its data access layer is abstracted so that it can be easily swapped out for a different database." +
-      "This was very important since the other teams are siloed and use different databases.",
-    tags: ["SQLServer", "WPF", "dotnet"],
+    description: "This is a lightweight barcode scanning station that I built while at Elemental Scientific. " +
+      "It uses Entity Framework Core and WPF. It is a simple tool that allows users to use a barcode scanner to associate vial barcodes with a sample rack. " +
+      "The application is very modular as its data access layer is abstracted so that it can be easily swapped out for a different database. " +
+      "This was very important since the other teams are siloed and use different databases. ",
+    tags: ["SQLServer", "WPF", ".NET"],
     titleImage: "/scan/title.png",
     images: [
       {src: "/scan/rack.png", description: "Rack with barcode scanning"},
